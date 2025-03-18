0b5601bae663485ea2fd13f3bb0e1709daab3a17
suyashshri <suyashshrivastava736@gmail.com>
2025-03-18 23:53:31 +0530

    adding more endpoints in BE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user";
+import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
+app.use(clerkMiddleware());
 app.use(express.json());
 app.use("api/v1/user", userRouter);
 
--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -1,23 +1,69 @@
 import { Router } from "express";
 import { CreateProjectType } from "../utils/types";
+import { prisma } from "../utils/db";
+import { getAuth, requireAuth } from "@clerk/express";
 
 const router = Router();
 
-router.post("/", (req, res) => {
+router.post("/", requireAuth(), async (req, res) => {
   const data = CreateProjectType.safeParse(req.body);
   if (!data.success) {
     res.status(400).json({
       message: "Incorrect inputs",
     });
+    return;
   }
+  const { userId } = getAuth(req);
+  if (!userId) {
+    res.status(403).json({
+      message: "Unauthorized, Please login first",
+    });
+    return;
+  }
+  const parsedData = data.data;
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name: parsedData?.name,
+        description: parsedData?.description,
+        visibility: parsedData?.visibility,
+        ownerId: userId,
+      },
+    });
 
-  const { name, description, visibility } = data.data;
+    res.status(201).json({
+      projectId: newProject.id,
+    });
+  } catch (error) {
+    console.log("Error in creating new project", error);
+  }
 });
 
-router.get("/", () => {});
+router.get("/", requireAuth(), async (req, res) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) {
+      res.status(403).json({
+        message: "Unauthorized, Please login first",
+      });
+      return;
+    }
+    const allProjects = await prisma.project.findMany({
+      where: {
+        id: userId,
+      },
+    });
 
-router.get("/:projectId", () => {});
+    res.status(201).json({
+      projects: allProjects,
+    });
+  } catch (error) {
+    console.log("Error in getting all projects", error);
+  }
+});
 
-router.delete("/:projectId", () => {});
+router.get("/:projectId", requireAuth(), async (req, res) => {});
+
+router.delete("/:projectId", requireAuth(), async (req, res) => {});
 
 export default router;
